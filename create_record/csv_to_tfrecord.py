7ea4a78e0d661464a64a4a6a2d93c91048913bd0
mohak <30952926+mohak1@users.noreply.github.com>
2020-05-24 15:30:23 +0530

    Add files via upload

---
+++ create_record/csv_to_tfrecord.py
@@ -0,0 +1,128 @@
+"""
+Usage:
+  # From tensorflow/models/
+  # Create train data:
+  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
+
+  # Create test data:
+  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import io
+import pandas as pd
+import tensorflow as tf
+
+from PIL import Image
+import sys
+#give the path of object_detection folder below.
+sys.path.append('/home/mohak/Documents/models-master/research/')
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+
+flags = tf.app.flags
+flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
+flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_string('image_dir', '', 'Path to images')
+FLAGS = flags.FLAGS
+
+
+# TO-DO replace this with label map
+def class_text_to_int(row_label):
+    #print(row_label)
+    if row_label == 'Button':
+        return 1
+
+    if row_label == 'Label':
+        return 2
+
+    if row_label == 'CheckBox':
+        return 3
+
+    if row_label == 'RadioButton':
+        return 4
+
+    if row_label == 'Image':
+        return 5
+
+    if row_label == 'TextBox':
+        return 6
+
+    
+def split(df, group):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
+
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def main(_):
+    '''
+    FLAGS.output_path = "./frame_images_voc_2_v2_v1.record"
+    FLAGS.image_dir = "/home/tejesh/projects/python/sub_assembly_tvs/frame_images_resized/"
+    FLAGS.csv_input = "/home/tejesh/projects/python/sub_assembly_tvs/frame_images_voc_2_v2/frame_images_voc_2_v2_v1.csv"
+    '''
+
+    FLAGS.output_path = "/home/mohak/Music/implementation/record_files/processed_dataset.record"
+    FLAGS.image_dir = "/home/mohak/Music/implementation/preprocessed_dataset/"
+    FLAGS.csv_input = "/home/mohak/Music/implementation/csv_files/processed_dataset.csv"
+
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    path = os.path.join(FLAGS.image_dir)
+    examples = pd.read_csv(FLAGS.csv_input)
+    grouped = split(examples, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, path)
+        writer.write(tf_example.SerializeToString())
+
+    writer.close()
+    output_path = os.path.join(os.getcwd(), FLAGS.output_path)
+    print('Successfully created the TFRecords: {}'.format(output_path))
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ create_record/xml_to_csv.py
@@ -0,0 +1,40 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+
+def main(path):
+    #image_path = os.path.join(os.getcwd(), 'annotations')
+    image_path = path
+    xml_df = xml_to_csv(image_path)
+    #name of the csv file
+    xml_df.to_csv('processed_dataset.csv', index=None)
+    print('Successfully converted xml to csv.')
+
+
+#give the path to xml files like this:
+#main('/path to xml files')
+
+main('/home/mohak/Music/implementation/xml_dataset')
